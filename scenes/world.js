972d03de85165f692510dae02197e504e691b60f
Your NameRyan <inoober2008@gmail.com>
2025-10-17 22:47:37 -0400

    Got the core gameplay down i think you can hit the spawning blocks, imma add music now

--- scenes/world.js
+++ scenes/world.js
@@ -1,35 +1,46 @@
 function setWorld(worldState) {
     const leftHand = add([
         sprite('g-left'),
-        area(),
+        area({
+            shape: new Rect(vec2(0, 0), 30, 55),
+            offset: vec2(15, 5),
+        }),
         body({isStatic: true}),
         pos(475, 720 - 200),
         scale(1.5),
         'left']);
     const rightHand = add([
         sprite('g-right'),
-        area(),
+        area({
+            shape: new Rect(vec2(0, 0), 30, 55),
+            offset: vec2(15, 5),
+        }),
         body({isStatic: true}),
         pos(745, 720 - 200),
         scale(1.5),
         'right']);
     const upHand = add([
         sprite('g-up'),
-        area(),
+        area({
+            shape: new Rect(vec2(0, 0), 30, 55),
+            offset: vec2(15, 5),
+        }),
         body({isStatic: true}),
         pos(655, 720 - 200),
         scale(1.5),
         'up']);
     const downHand = add([
         sprite('g-down'),
-        area(),
+        area({
+            shape: new Rect(vec2(0, 0), 30, 55),
+            offset: vec2(15, 5),
+        }),
         body({isStatic: true}),
         pos(565, 720 - 200),
         scale(1.5),
         'down']);
     const leftRGB = add([
         sprite('rgb-left'),
-        area(),
         body({isStatic: true}),
         pos(475, 720 - 206),
         scale(1.5),
@@ -37,7 +48,6 @@ function setWorld(worldState) {
         'rgb-left']);
     const rightRGB = add([
         sprite('rgb-right'),
-        area(),
         body({isStatic: true}),
         pos(745, 720 - 209),
         scale(1.5),
@@ -45,7 +55,6 @@ function setWorld(worldState) {
         'rgb-right']);
     const upRGB = add([
         sprite('rgb-up'),
-        area(),
         body({isStatic: true}),
         pos(655, 720 - 210),
         scale(1.5),
@@ -53,7 +62,6 @@ function setWorld(worldState) {
         'rgb-up']);
     const downRGB = add([
         sprite('rgb-down'),
-        area(),
         body({isStatic: true}),
         pos(565, 720 - 209),
         scale(1.5),
@@ -69,23 +77,6 @@ function setWorld(worldState) {
 
     let activeDir = null;
 
-    onUpdate( () => {
-        if (activeDir === 'left') {
-            leftRGB.opacity = 1;
-        } else if (activeDir === 'right') {
-            rightRGB.opacity = 1;
-        } else if (activeDir === 'up') {
-            upRGB.opacity = 1;
-        } else if (activeDir === 'down') {
-            downRGB.opacity = 1;
-        }
-        else {
-            leftRGB.opacity = 0;
-            rightRGB.opacity = 0;
-            upRGB.opacity = 0;
-            downRGB.opacity = 0;
-        }
-    });
 
     const options = ['left','down','up','right'];
     const xOptionPos = [475,565,655,745]
@@ -103,14 +94,19 @@ function setWorld(worldState) {
 
     function spawnSignals() {
         const index = getRandomInt(0, options.length);
-        const type = options[index];
+        const arrowType = options[index];
         const xPos = xOptionPos[index];
         const arrow = add([
-            sprite(`rgb-${type}`),
-            area(),
+            sprite(`rgb-${arrowType}`),
+            area({
+                shape: new Rect(vec2(0, 0), 30, 55),
+                offset: vec2(15, 5),
+            }),
             pos(xPos, 720 - 600),
             scale(1.5),
-            { speed: fallSpeed },
+            {   speed: fallSpeed,
+                type: arrowType,
+             },
             'signal',
         ]);
 
@@ -123,15 +119,41 @@ function setWorld(worldState) {
 
     onUpdate('signal', (arrow) => {
         arrow.move(0, arrow.speed);
-        if (arrow.pos.y > 720 + 64) {
-            destroy(arrow);
+        if (arrow.pos.y > 720) {
+            destroy(arrow)
         }
     });
-    onCollide('signal', 'up', (signal, up) => {
-        debug.log(signal.pos.y);
-        destroy(signal)
+
+    onUpdate(() => {
+        leftRGB.opacity = rightRGB.opacity = upRGB.opacity = downRGB.opacity = 0;
+        if (activeDir === 'left') leftRGB.opacity = 1;
+        else if (activeDir === 'right') rightRGB.opacity = 1;
+        else if (activeDir === 'up') upRGB.opacity = 1;
+        else if (activeDir === 'down') downRGB.opacity = 1;
     });
 
+    let dirTime = 0
+    const perfectTime = 0.72;
+    const okTime = perfectTime - 20 || perfectTime + 20;
+    const goodTime = perfectTime - 15 || perfectTime + 15;
+    const greatTime = perfectTime - 10 || perfectTime + 10;
+
+    for (const dir of options) {
+        onCollideUpdate('signal', dir, (signal, hand) => {
+            dirTime += dt();
+            if (activeDir === dir && signal.type === dir) {
+                let roundedTime = dirTime.toFixed(2);
+                if (roundedTime === perfectTime){
+                    debug.log('Perfect!', roundedTime);
+                } else {
+                    debug.log('Meh...', roundedTime);
+                }
+                destroy(signal);
+                dirTime = 0;
+            }
+        });
+    }
+    debug.inspect = true;
     for (const key of Object.keys(keyDirMap)) {
         onKeyDown(key, () => {
             if (activeDir) return;
